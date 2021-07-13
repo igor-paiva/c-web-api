33524ea6f787345db14c05a7be98c7e7eb064e3b
igor-paiva <igorbatistapaiva@outlook.com>
2021-07-13 14:09:32 -0300

    Add more states to state module

--- inc/states.h
+++ inc/states.h
@@ -4,19 +4,30 @@
 #include <stdio.h>
 #include <limits.h>
 
+#define TRUE 1
+#define FALSE 0
+
+typedef int boolean;
+
 /* general status, could be either a success_t or an error_t */
 typedef int state;
 
 /* those will always be positive */
 typedef enum {
   SUCCESS = 1,
-  CREATED
+  CREATED,
+  UPDATED,
+  DELETED
 } success_t;
 
 /* those will always be negative */
 typedef enum {
- UNABLE_OPEN_FILE = INT_MIN,
+ NOT_FOUND = INT_MIN,
+ ALLOC_FAIL,
+ UNABLE_OPEN_FILE,
+ UNABLE_READ_FILE,
  UNABLE_WRITE_FILE,
+ UNABLE_CREATE_FILE,
 } error_t;
 
 boolean is_error(state value);
